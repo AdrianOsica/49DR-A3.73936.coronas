145e6e36f308db8bc038d1577e5ec2dfd8ecbdc0
AdrianOsica <43826307+AdrianOsica@users.noreply.github.com>
2021-01-28 14:13:48 +0100

    commit 1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace coronas
+{
+
+    class Program
+    {
+
+        private static readonly HttpClient client = new HttpClient();
+        
+        static async Task Main(string[] args)
+        {
+
+            using var client = new HttpClient();
+
+            string content = await client.GetStringAsync("https://restcountries.eu/rest/v2/name/Poland");
+           
+        
+            dynamic stuff = JsonConvert.DeserializeObject(content);
+            Console.WriteLine(stuff[0].capital);
+        
+
+            //string state;
+            //Console.WriteLine("Podaj Panstwo");
+            //state = Console.ReadLine();
+
+
+        }
+
+    }
+}
