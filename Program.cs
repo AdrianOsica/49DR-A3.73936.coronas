318a75bd0969b8b773152ab27630e91a580c9635
AdrianOsica <43826307+AdrianOsica@users.noreply.github.com>
2021-01-29 13:00:14 +0100

    commit 2

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Net;
+using System.Net.Mail;
 
 namespace coronas
 {
@@ -19,17 +21,52 @@ namespace coronas
 
             using var client = new HttpClient();
 
-            string content = await client.GetStringAsync("https://restcountries.eu/rest/v2/name/Poland");
-           
+            Console.WriteLine("Podaj Panstwo");
+            string country = Console.ReadLine();
+            
+            string get_capitol = await client.GetStringAsync($"https://restcountries.eu/rest/v2/name/{country}");
+            Console.Clear();
         
-            dynamic stuff = JsonConvert.DeserializeObject(content);
-            Console.WriteLine(stuff[0].capital);
-        
+            dynamic capitol_api = JsonConvert.DeserializeObject(get_capitol);
+            
+            string get_weather = await client.GetStringAsync($"http://api.openweathermap.org/data/2.5/weather?q={capitol_api[0].capital}&appid=4d9a35918230b98cdfd37d30dca37ca3");
+            dynamic weather = JsonConvert.DeserializeObject(get_weather);
 
-            //string state;
-            //Console.WriteLine("Podaj Panstwo");
-            //state = Console.ReadLine();
+            string get_covid = await client.GetStringAsync($"https://api.covid19api.com/live/country/{country}/status/confirmed");
+            dynamic covid = JsonConvert.DeserializeObject(get_covid);
 
+            Console.WriteLine($"Stolica kraju ktorego wybrales jest {capitol_api[0].capital}");
+            Console.WriteLine($"Szerokosc i dlugosc geograficzna: {covid[14].Lat} {covid[14].Lon} \n \n ");
+
+            Console.WriteLine($"W {capitol_api[0].capital} jest obecnie {weather.weather[0].main} i potwierdzonych zakazen covid-19 {covid[14].Confirmed}");
+
+
+            var fromAddress = new MailAddress("jareckijacek37@gmail.com", "From Name");
+            var toAddress = new MailAddress("osica32@gmail.com", "To Name");
+            const string fromPassword = "";
+            const string subject = "Subject";
+            const string body = "Body";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+
+
+          
 
         }
 
